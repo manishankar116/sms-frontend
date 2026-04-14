6ccffcbd348cb719984045b34ab409b8d8dfebf0
manishankar116 <manishankar7675@gmail.com>
2026-04-14 13:24:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        Hello World
+      </section>
+    </>
+  )
+}
+
+export default App
